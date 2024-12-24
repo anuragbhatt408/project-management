b8164aa3502f218e90be8e1ffca09f0b5b71a6c4
anuragbhatt408 <anuragbhatt408@gmail.com>
2024-12-24 15:15:32 +0530

    s3 images

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'projectmanagement-s3.s3.us-east-1.amazonaws.com/public/',
+        port:"",
+        pathname: '/**',
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
